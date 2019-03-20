395d48ecc9d2d685f98316b5bc9baff73a15c51a
myost <mayo9279@colorado.edu>
2019-03-20 13:41:59 -0600

    Ref #7: changed node files to read in the ejs file with the landing page

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/account', accountRouter);
 app.use('/account_awards', accountAwardsRouter);
