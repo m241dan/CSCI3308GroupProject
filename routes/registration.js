7d3454a6a323b3721a884487778090213ef62730
myost <mayo9279@colorado.edu>
2019-03-22 16:04:54 -0600

    Ref #7: set up ejs files for login, sign up, and add workout pages to render in node using respective routers

--- routes/registration.js
+++ routes/registration.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-    res.write('This is /registration');
-    res.end();
+router.get('/', function(req, res, next) {
+    // res.write('This is /registration');
+    // res.end();
+    res.render('register', {
+  	title: "F.I.T. Fam - Sign Up"
+  });
 });
 
 module.exports = router;
