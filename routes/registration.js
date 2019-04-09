0a062c50aeeb8376f3b9121eb87f9bf8f8e687c1
xita3423 <47006496+xita3423@users.noreply.github.com>
2019-04-09 10:48:51 -0600

    Update registration.js

--- routes/registration.js
+++ routes/registration.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
+var db = require('../database/dbHandle');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-    res.write('This is /registration');
-    res.end();
+router.get('/', function(req, res, next) {
+    // res.write('This is /registration');
+    // res.end();
+    res.render('register', {
+  	title: "F.I.T. Fam - Sign Up"
+  });
+});
+
+router.post('/', async (req, res) => {
+    console.log( "pwd: " + req.body.passwordFirst );
+    var pwd = await bcrypt.hash(req.body.passwordFirst, 5);
+    db.insertUser(req.body.firstName, req.body.lastName, req.body.userName, req.body.emailAddress, pwd);
+    res.redirect('/');
 });
 
 module.exports = router;
