1420bf066ea541496c55dfd1d0bd000a3d3f7773
xita3423 <47006496+xita3423@users.noreply.github.com>
2019-04-15 21:24:05 -0600

    added check duplicates

--- routes/registration.js
+++ routes/registration.js
@@ -8,15 +8,29 @@ router.get('/', function(req, res, next) {
     // res.write('This is /registration');
     // res.end();
     res.render('register', {
-  	title: "F.I.T. Fam - Sign Up"
+    title: "F.I.T. Fam - Sign Up"
   });
 });
 
 router.post('/', async (req, res) => {
     console.log( "pwd: " + req.body.passwordFirst );
     var pwd = await bcrypt.hash(req.body.passwordFirst, 5);
-    db.insertUser(req.body.firstName, req.body.lastName, req.body.userName, req.body.emailAddress, pwd);
-    res.redirect('/');
+
+    
+    var checkemail = req.body.emailAddress;
+    var count = 'SELECT COUNT(1) FROM enjoyer WHERE email = 'checkemail';';
+    if(count == 0)
+    {
+        // NO Duplicates in database 
+         db.insertUser(req.body.firstName, req.body.lastName, req.body.userName, req.body.emailAddress, pwd);
+    }
+    else
+    {
+        //redirect? or display error message?
+        res.redirect('/');
+    }
+   
+    
 });
 
 module.exports = router;
