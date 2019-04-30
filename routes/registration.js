6704052288ba783833afea320f5fbc3d7d172da0
Dan Koris <korisd@gmail.com>
2019-04-30 10:44:17 -0600

    Ref #25: Deleted excess console.log() messages

--- routes/registration.js
+++ routes/registration.js
@@ -5,15 +5,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // res.write('This is /registration');
-    // res.end();
     res.render('register', {
   	title: "F.I.T. Fam - Sign Up"
   });
 });
 
 router.post('/', async (req, res) => {
-    console.log( "pwd: " + req.body.passwordFirst );
     var pwd = await bcrypt.hash(req.body.passwordFirst, 5);
     db.insertUser(req.body.firstName, req.body.lastName, req.body.userName, req.body.emailAddress, pwd);
     res.redirect('/');
